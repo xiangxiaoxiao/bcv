7bb82fa218f8a602c08fd982ea944ecb17cb40e2
vasiliy_ <karasev00@gmail.com>
2015-06-28 01:29:47 -0700

    updated tvdeblurring

--- examples/test_tvdeblur.cpp
+++ examples/test_tvdeblur.cpp
@@ -8,20 +8,24 @@
 
 using namespace std;
 
-DEFINE_string(input, "../images/peppers_blur_rgb.png", "Input image filename");
+DEFINE_string(input, "../images/blur/lena_blur.png", "Input image filename");
 DEFINE_string(output, "out.png", "Output image filename");
 DEFINE_double(lambda, 0.5, "TV penalty");
 DEFINE_int32(kernel_size, 15, "kernel size");
 DEFINE_int32(verbosity, 100, "verbosity");
-DEFINE_int32(max_iterations, 1, "max iterations");
+DEFINE_int32(max_inner_iterations, 1, "max inner iterations");
+DEFINE_int32(max_nonblind_iterations, 100, "max nonblind iterations");
 DEFINE_int32(max_anneal_rounds, 10000, "max anneal iterations");
 // pyramid parameters
+DEFINE_double(annealing_rate, 0.999, "annealing rate");
 DEFINE_int32(pyramid_num_levels, 1, "num pyramid levels");
 DEFINE_double(pyramid_imsize_scale, 0.90, "pyramid image scaling");
 DEFINE_double(pyramid_lambda_scale, 1.90, "pyramid lambda scaling");
 // gradient step parameters
 DEFINE_double(grad_descent_step_u, 5e-3, "pyramid image scaling");
 DEFINE_double(grad_descent_step_k, 1e-3, "pyramid lambda scaling");
+DEFINE_double(dfx_tolerance, 1e-5f, "pyramid lambda scaling");
+DEFINE_bool(grayscale, false, "solve grayscale?");
 
 int main(int argc, char** argv) { 
     gflags::SetUsageMessage("TV-regularized segmentation example");
@@ -31,20 +35,22 @@ int main(int argc, char** argv) {
     vector<float> img = bcv::bcv_imread<float>(FLAGS_input.c_str(), &rows, &cols, &chan);
     transform(img.begin(), img.end(), img.begin(), 
                     bind1st( multiplies<float>(), 1.0f/256.0f ) );
-
+    if ((FLAGS_grayscale) && (chan==3)) { 
+        img = bcv::rgb2gray(img); chan =1; }
     printf("Loaded from '%s'\n", FLAGS_input.c_str() );
     bcv::tvdeblur_params params;
 
     params.lambda = FLAGS_lambda;
     params.min_lambda = 1e-4;
-    params.annealing_rate = 0.999f;
-    params.max_iterations = FLAGS_max_iterations;
-    params.max_anneal_rounds = FLAGS_max_anneal_rounds;
+    params.annealing_rate = FLAGS_annealing_rate;
+    params.max_inner_iterations = FLAGS_max_inner_iterations;
+    params.max_nonblind_iterations = FLAGS_max_nonblind_iterations;
+    params.max_anneal_rounds = FLAGS_max_anneal_rounds;    
     params.grad_descent_step_u = FLAGS_grad_descent_step_u;
     params.grad_descent_step_k = FLAGS_grad_descent_step_k;
     params.eps = 1e-5f;
     params.ker_size = FLAGS_kernel_size;
-    params.dfx_tolerance = 1e-5f;
+    params.dfx_tolerance = FLAGS_dfx_tolerance;
     params.verbosity = FLAGS_verbosity;
     params.pyramid_numlevels = FLAGS_pyramid_num_levels;
     params.pyramid_imsize_scaling = FLAGS_pyramid_imsize_scale;
@@ -56,15 +62,15 @@ int main(int argc, char** argv) {
     // ------------------------------------------------------------------------
     t1 = bcv::now_ms();
     bcv::tvdeblur tvdb(img, params);
+    
     tvdb.solve();
     vector<float> out = tvdb.result();
     t2 = bcv::now_ms();
 
     transform(out.begin(), out.end(), out.begin(), 
                     bind1st( multiplies<float>(), 256.0f ) );
-    bcv::bcv_imwrite<float>(FLAGS_output.c_str(), out, rows, cols, chan);
+    bcv::bcv_imwrite(FLAGS_output.c_str(), out, rows, cols, chan);
     printf("Wrote the result to '%s'\n", FLAGS_output.c_str() );
     printf("took %.3f s\n", (t2-t1)/1000.0f );
-    
     return 0;
 }
